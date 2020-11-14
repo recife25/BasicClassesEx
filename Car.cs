31d9a4b4e0a8c15c11d27297134d5111f6fe4155
recife25 <felipe.j.ruiz64@gmail.com>
2020-11-13 22:35:09 -0500

    Added classes exercise

---
+++ Car.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicClassesExercise
+{
+    public class Car
+    {
+        public Car()
+        {
+
+        }
+        //multiple contructors - using the concept of method overloading 
+        //public Car (string make, string model, int year)//ready to accept three parameters two of string and one of int
+        public Car (string makeInput, string modelInput, int yearInput)
+        {
+            //Make ref property make ref to parameter
+            //Make = make;
+            // Model = model;
+            // Year = year;
+            Make = makeInput;
+            Model = modelInput;
+            Year = yearInput;
+
+
+        }
+
+        //constructors
+        // constructor has a special member method that has the same name as its class
+        //fields 
+        //properties
+        //methods 
+        //create a Make property of type string that is public
+        public string Make { get; set; } //read = read set = write
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿//Felipe Ruiz - aka Phil 
+//classes 
+using System;
+using System.Collections.Generic;
+
+
+namespace BasicClassesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //here is my Main Method 
+            // var myCar = new Car(); //new Car(); here is a constructor special member method which helps us create a new instance of the Car class 
+            //dot notation 
+            Car myCar = new Car();
+            myCar.Make = "Ford";
+            myCar.Model = "Focus";
+            myCar.Year = 2013;
+
+            var Toyota = new Car()//another way to store value in an object property
+            //object initializer 
+            {
+                Make = "Toyota",
+                Model = "Avalon",
+                Year = 1995
+            };
+            //passing it through the constructor
+            //with two overloads with or without parameters 
+            var chevy = new Car("Chevy", "Impala", 2001);
+
+            var carList = new List<Car>() { myCar, Toyota, chevy, new Car(), new Car()};
+            //Console.WriteLine(carList.Capacity);
+            foreach (var vehicle in carList)
+            {
+                Console.WriteLine($"{vehicle.Make} {vehicle.Model} {vehicle.Year}");
+            }
+
+
+        }
+    }
+}
